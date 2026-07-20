7ec067b5efcdb44715bee9aea67867c5b83b9fc9
enzogran01 <yournarrator013@gmail.com>
2026-07-20 16:11:30 -0300

    :wrench: refactor: belongs to many reference in user model

--- app/Models/User.php
+++ app/Models/User.php
@@ -11,6 +11,7 @@ use Illuminate\Database\Eloquent\Attributes\Hidden;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 use Spatie\Permission\Traits\HasRoles;
 
 #[Fillable(['name', 'email', 'password', 'telefone', 'avatar'])]
