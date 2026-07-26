d96ad60f74147227fca89eddf15df798fb0754e6
enzogran01 <yournarrator013@gmail.com>
2026-07-26 14:48:14 -0300

    :lipstick: style: update notification badge to look like discord

--- resources/views/livewire/inbox-notifications.blade.php
+++ resources/views/livewire/inbox-notifications.blade.php
@@ -1,15 +1,14 @@
 <div>
     <x-filament::modal id="notifications-modal" width="lg">
         <x-slot name="trigger">
-            <div class="relative">
+            <div class="relative inline-flex mx-4 mt-1">
                 <x-filament::icon-button
                     icon="heroicon-o-inbox"
                     color="gray"
                     tooltip="Notificações"
-                    class="mx-4"
                 />
                 @if($this->notifications->count() > 0)
-                    <span class="absolute top-0 right-4 flex items-center justify-center min-w-[1.25rem] h-5 rounded-full bg-danger-600 text-xs font-bold text-white px-1">
+                    <span class="absolute top-0 right-0 transform translate-x-1/4 -translate-y-1/4 flex items-center justify-center min-w-[1.25rem] h-5 rounded-full bg-red-600 text-[10px] font-bold text-white px-1 border-2 border-white dark:border-gray-900 shadow-sm">
                         {{ $this->notifications->count() }}
                     </span>
                 @endif
